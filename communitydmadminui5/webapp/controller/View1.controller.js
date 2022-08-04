e5e58899d1ddb40ce8e32154f4204a5ac3aa6358
Giampaolo Bolzonella <giampaolo.bolzonella@horsa.it>
2022-08-04 12:26:56 +0000

    Fix controller

--- communitydmadminui5/webapp/controller/View1.controller.js
+++ communitydmadminui5/webapp/controller/View1.controller.js
@@ -1,15 +1,21 @@
-sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
-	/**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-	function (Controller) {
-		"use strict";
-
-		return Controller.extend("ns.communitydmadminui5.controller.View1", {
-			onInit: function () {
-
-			}
-		});
-	});
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+    "use strict";
+    return Controller.extend("com.sap.sample.sdm", {
+        onInit: function () {
+            /* OPTIONAL: pass the same settings here, if not added in 'manifest.json' as per previous steps */
+            //this.getView().byId("sdi-container").setSettings({
+            //    "repositoryId": "958dc972-c845-44ff-8a97-4badab151bfe", // REPOSITORY_ID
+            //    "objectId": "b4725a7000298277c993cc26", // OBJECT_ID
+            //    "forceLoad": false
+            //});
+        },
+        // registered event handler for 'componentCreated' event of Component Container
+        onComponentCreated: function (oEvent) {
+            this._oDocumentTable = oEvent.getParameter("component");
+            /* OPTIONAL: set a folder as home folder */
+            //this._oDocumentTable.setHomeFolder("b4725a7000298277c993cc26"); // OBJECT_ID
+            /* OPTIONAL: request a navigation to a repository & folder during runtime */
+            //this._oDocumentTable.requestNavigationAndOpen("958dc972-c845-44ff-8a97-4badab151bfe", "b4725a7000298277c993cc26"); // REPOSITORY_ID, OBJECT_ID
+        }
+    });
+});
